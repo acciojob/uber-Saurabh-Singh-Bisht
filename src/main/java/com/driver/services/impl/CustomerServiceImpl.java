b7aa12ca4f4f536b0432d2a2dfd0be1106a311fb
Saurabh Singh Bisht <bisht.singh750@gmail.com>
2023-06-20 18:40:59 +0530

    exception corrected

--- src/main/java/com/driver/services/impl/CustomerServiceImpl.java
+++ src/main/java/com/driver/services/impl/CustomerServiceImpl.java
@@ -57,7 +57,7 @@ public class CustomerServiceImpl implements CustomerService {
 			}
 		}
 		if(driver == null){
-			throw new Exception("No cab available!");
+			throw new Exception("No value present");
 		}
 		TripBooking tripBooking = new TripBooking();
 		tripBooking.setFromLocation(fromLocation);
